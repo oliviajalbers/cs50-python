ffa2b08b589bd954894c54014bdc4ad382412b68
Olivia Albers <88689331+oliviajalbers@users.noreply.github.com>
2023-05-12 16:19:08 -0500

    Create deep.py

---
+++ week_1/deep/deep.py
@@ -0,0 +1,9 @@
+def main():
+    answer = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+    answer = answer.lower().strip()
+    if answer == "42" or answer == "forty-two" or answer == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+main()
