da5706dd16f35b0c8f607a16abc7f39e5120788d
Olivia Albers <88689331+oliviajalbers@users.noreply.github.com>
2023-09-24 19:48:14 -0500

    Create nutrition.py

---
+++ week_2/nutrition/nutrition.py
@@ -0,0 +1,32 @@
+def main():
+    fruits = [
+        {"fruit": "apple", "calories": 130},
+        {"fruit": "avocado", "calories": 50},
+        {"fruit": "banana", "calories": 110},
+        {"fruit": "cantaloupe", "calories": 50},
+        {"fruit": "grapefruit", "calories": 60},
+        {"fruit": "grapes", "calories": 90},
+        {"fruit": "honeydew melon", "calories": 50},
+        {"fruit": "kiwifruit", "calories": 90},
+        {"fruit": "lemon", "calories": 15},
+        {"fruit": "lime", "calories": 20},
+        {"fruit": "nectarine", "calories": 60},
+        {"fruit": "orange", "calories": 80},
+        {"fruit": "peach", "calories": 60},
+        {"fruit": "pear", "calories": 100},
+        {"fruit": "pineapple", "calories": 50},
+        {"fruit": "plums", "calories": 70},
+        {"fruit": "strawberries", "calories": 50},
+        {"fruit": "sweet cherries", "calories": 100},
+        {"fruit": "tangerine", "calories": 50},
+        {"fruit": "watermelon", "calories": 80}
+    ]
+    item = input("Item: ")
+    item = item.lower()
+    for fruit in fruits:
+        if fruit["fruit"] == item:
+            calories = fruit["calories"]
+            print("Calories: " + str(calories))
+            break
+
+main()
